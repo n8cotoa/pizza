d35b1503289894f02d8a7f2bcb94515db2b93c47
Nate Cottle <cottle.nate@gmail.com>
2018-06-15 09:07:16 -0700

    pizza form is completed, add js to get form values and create object.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,19 @@
-function MyPizza() {
-  this.toppings = []
-  this.size = ""
-  this.crust = ""
+function MyPizza(size, toppings, crust) {
+  this.size = size;
+  this.toppings = toppings;
+  this.crust = crust;
 }
 
 $(document).ready(function(){
-  var pizza = new MyPizza (size, toppings, crust)
+  $("#pizzaBtn").click(function(e){
+    e.preventDefault()
+    var size = document.getElementById("size").value
+    var crust = $("#crust").val()
+    var toppings = []
+      $("input:checkbox[name=topping]:checked").each(function(){
+      toppings.push($(this).val())
+    });
+    var pizza = new MyPizza(size, toppings, crust)
+    console.log(pizza);
+  });
 });
